6b7d20bd16e9dac18ceb98ce54d09fead55f8ecb
panjunwen <raool@foxmail.com>
2022-09-19 00:48:14 +0800

    fix: production cannot load file

--- web/.umirc.ts
+++ web/.umirc.ts
@@ -2,4 +2,8 @@ export default {
   npmClient: "yarn",
   tailwindcss: {},
   plugins: ["@umijs/plugins/dist/tailwindcss"],
+  publicPath: process.env.NODE_ENV === "development" ? undefined : "./",
+  history: {
+    type: "hash",
+  },
 };
