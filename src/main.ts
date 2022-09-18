d7fa501a389393c2c1c6ba66b89c69dac6bbf0ca
panjunwen <raool@foxmail.com>
2022-09-19 00:16:50 +0800

    fix: more new window error

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,27 @@ import xlsxHandler from "./xlsxHandler";
 
 const isDev = process.env.ELECTRON_NODE_ENV === "dev";
 
+// 业务逻辑
+function getFileName() {
+  const result = dialog.showOpenDialogSync({
+    properties: ["openFile"],
+    filters: [{ name: "xlsx", extensions: ["xlsx"] }],
+  });
+  return result?.[0];
+}
+function getDirName() {
+  const result = dialog.showOpenDialogSync({
+    properties: ["openDirectory"],
+  });
+  return result?.[0];
+}
+function alert(message: string) {
+  dialog.showMessageBoxSync({ type: "info", message });
+}
+
+// electron 逻辑
 function createWindow() {
   const win = new BrowserWindow({
-    // width: 1280,
-    // height: 720,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -20,33 +37,15 @@ function createWindow() {
   } else {
     win.loadFile(path.join(__dirname, "..", "web", "dist", "index.html"));
   }
+}
 
-  // 业务逻辑
-  function getFileName() {
-    const result = dialog.showOpenDialogSync({
-      properties: ["openFile"],
-      filters: [{ name: "xlsx", extensions: ["xlsx"] }],
-    });
-    return result?.[0];
-  }
-  function getDirName() {
-    const result = dialog.showOpenDialogSync({
-      properties: ["openDirectory"],
-    });
-    return result?.[0];
-  }
-  function alert(message: string) {
-    dialog.showMessageBoxSync({ type: "info", message });
-  }
+app.whenReady().then(() => {
+  createWindow();
 
   ipcMain.handle("getFileName", getFileName);
   ipcMain.handle("getDirName", getDirName);
   ipcMain.handle("alert", (e, message: string) => alert(message));
   ipcMain.handle("xlsxHandler", (e, ...args) => xlsxHandler(...args));
-}
-
-app.whenReady().then(() => {
-  createWindow();
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
